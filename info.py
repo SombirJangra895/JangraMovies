f3a5891046510e23f750d82971b5c02abc7e634b
SombirJangra895 <157369388+SombirJangra895@users.noreply.github.com>
2024-01-23 21:00:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -45,8 +45,8 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://sombirjangra895:Sj#8221916213@jangra.0g7v6jj.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "jangra")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://sombirjangra213:Sj#8221916213@cluster0.xgxvv51.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
